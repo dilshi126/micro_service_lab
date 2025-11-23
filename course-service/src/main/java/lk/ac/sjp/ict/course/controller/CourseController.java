33e691529751d4da17c873ea3d1eeafe945c8d70
dilshi126 <dilshi126@users.noreply.github.com>
2025-11-23 06:50:15 +0530

    Add Course Service with CRUD operations (Port 8082)

---
+++ course-service/src/main/java/lk/ac/sjp/ict/course/controller/CourseController.java
@@ -0,0 +1,33 @@
+package lk.ac.sjp.ict.course.controller;
+
+import lk.ac.sjp.ict.course.model.Course;
+import lk.ac.sjp.ict.course.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Course createCourse(@RequestBody Course course) {
+        return courseService.createCourse(course);
+    }
+}
---
+++ course-service/src/main/java/lk/ac/sjp/ict/course/model/Course.java
@@ -0,0 +1,18 @@
+package lk.ac.sjp.ict.course.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String code;
+}
